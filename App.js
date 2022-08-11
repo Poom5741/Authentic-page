68cebef4bb8d6ee0730083f450c7774d3c09e818
poom <poom..@Pooms-MacBook-Pro.local>
2022-08-11 21:45:25 +0700

    first page ready

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SignInScreen from "./src/screens/SigninScreen/SignInScreen";
+// import Amplify from "aws-amplify";
+// import config from "./src/aws-exports";
+
+// Amplify.configure(config);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <SignInScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F9FBFC",
   },
 });
---
+++ src/screens/SigninScreen/SignInScreen.js
@@ -0,0 +1,109 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import Logo from "../../../assets/images/Logo_1.png";
+import Custominput from "../../component/Custominput";
+import CustomButton from "../../component/Custombutton/CustomButton";
+
+const SignInScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { height } = useWindowDimensions();
+
+  const onSignInPressed = () => {
+    console.warn("Sign in");
+  };
+
+  const onForgotPasswordPressed = () => {
+    console.warn("onForgotPasswordPressed");
+  };
+
+  const onSignInFacebook = () => {
+    console.warn("onSignInFacebook");
+  };
+  const onSignInGoogle = () => {
+    console.warn("onSignInGoogle");
+  };
+  const onSignInApple = () => {
+    console.warn("onSignInApple");
+  };
+
+  const onSignUpPress = () => {
+    console.warn("onSignUpPress");
+  };
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, { height: height * 0.3 }]}
+          resizeMode="contain"
+        />
+
+        <Custominput
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+        />
+        <Custominput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextExtry={true}
+        />
+        <CustomButton text="Sign In" onPress={onSignInPressed} />
+
+        <CustomButton
+          text="Forgot password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
+        <CustomButton
+          text="Sign In with Facebook"
+          onPress={onSignInFacebook}
+          bgColor="#E7EAF4"
+          fgColor="#4765A9"
+        />
+        <CustomButton
+          text="Sign In with Google"
+          onPress={onSignInGoogle}
+          bgColor="#FAE9EA"
+          fgColor="#DD4D44"
+        />
+        <CustomButton
+          text="Sign In with Apple"
+          onPress={onSignInApple}
+          bgColor="#e3e3e3"
+          fgColor="#363636"
+        />
+        <CustomButton
+          text="Don't have an account? Create one"
+          onPress={onSignUpPress}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: "center",
+    padding: 20,
+  },
+  logo: {
+    width: "70%",
+    maxWidth: 300,
+    maxHeight: 100,
+  },
+});
+
+export default SignInScreen;
